1b6d20a87b8a16fc59d50c1d6b17d69074206da3
Edwin Okugbo <edwinokugbo@gmail.com>
2022-11-19 20:03:28 +0100

    minor update 8

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,3 @@
-// import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
-
-import GoogleMapReact from "google-map-react";
-
 const mapStyles = {
   width: "50%",
   height: "50%",
@@ -16,7 +12,7 @@ function Contact(props) {
           <hr className="my-5" />
           <p>
             <div className="text-lg font-bold mb-2">
-              Purpose Thoughts Project
+              staywifme_official Project
             </div>
             <div className="mb-1">
               <span className="text-orange-800 font-bold">Phone:</span>{" "}
@@ -24,82 +20,54 @@ function Contact(props) {
             </div>
             <div className="mb-1">
               <span className="text-orange-800 font-bold">Email:</span>{" "}
-              contact@Purpose Thoughts.com
+              contact@staywifme_official.com
             </div>
             <div className="mb-1">
               <span className="text-orange-800 font-bold">Facebook:</span>{" "}
               <a
-                href="https://facebook.com/Purpose Thoughts"
+                href="https://facebook.com/staywifme_official"
                 target="_blank"
                 rel="noreferrer"
               >
-                https://facebook.com/Purpose Thoughts
+                https://facebook.com/staywifme_official
               </a>
             </div>
             <div className="mb-1">
               <span className="text-orange-800 font-bold">Twitter:</span>{" "}
               <a
-                href="https://twitter.com/Purpose Thoughts"
+                href="https://twitter.com/staywifme_official"
                 target="_blank"
                 rel="noreferrer"
               >
-                https://twitter.com/Purpose Thoughts
+                https://twitter.com/staywifme_official
               </a>
             </div>
             <div className="mb-1">
               <span className="text-orange-800 font-bold">Instagram:</span>{" "}
               <a
-                href="https://instagram.com/Purpose Thoughts"
+                href="https://instagram.com/staywifme_official"
                 target="_blank"
                 rel="noreferrer"
               >
-                @Purpose Thoughts
+                @staywifme_official
               </a>
             </div>
             <div className="mb-1">
               <span className="text-orange-800 font-bold">Youtube:</span>{" "}
               <a
-                href="https://youtube.com/Purpose Thoughts"
+                href="https://youtube.com/staywifme_official"
                 target="_blank"
                 rel="noreferrer"
               >
-                https://youtube.com/Purpose Thoughts
+                https://youtube.com/staywifme_official
               </a>
             </div>
           </p>
         </div>
-        <div className="contact__box--right">
-          {/* <Map
-            google={props.google}
-            zoom={12}
-            style={mapStyles}
-            initialCenter={{ lat: 6.6039262, lng: 3.3327518 }}
-          >
-            <Marker
-              position={{ lat: 6.615570827198901, lng: 3.3761745839766446 }}
-            />
-          </Map> */}
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: "" }}
-            defaultCenter={'center'}
-            defaultZoom={'yes'}
-            yesIWantToUseGoogleMapApiInternals
-            onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
-          >
-            {/* <
-              lat={59.955413}
-              lng={30.337844}
-              text="My Marker"
-            /> */}
-          </GoogleMapReact>
-        </div>
+        <div className="contact__box--right">Coming Soon...</div>
       </div>
     </div>
   );
 }
-
-// export default GoogleApiWrapper({
-//   apiKey: "AIzaSyD92udZaH8WPWsFj9NmZYeghlMFl0Nentg",
-// })(Contact);
 
 export default Contact;
