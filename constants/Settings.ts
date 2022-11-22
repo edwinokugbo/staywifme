bcb0688eef8f6e3d40c304f1c55c9853a553aa14
Edwin Okugbo <edwinokugbo@gmail.com>
2022-11-22 16:00:42 +0100

    checking for issues 2

--- constants/Settings.ts
+++ constants/Settings.ts
@@ -1,5 +1,5 @@
 export const Settings = {
-  API_DATA_URL: "http://localhost:3000/api/",
+  // API_DATA_URL: "http://localhost:3000/api/",
 
-  // API_DATA_URL: "https://staywifme.vercel.app/api/",
+  API_DATA_URL: "https://staywifme.vercel.app/api/",
 };
